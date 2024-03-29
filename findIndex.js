97b51c3f1fc29da2b7d77980541cceaff3b981c0
DevRahat <dev.rahatislam@gmail.com>
2024-03-30 01:25:12 +0600

    4th commit

--- findIndex.js
+++ findIndex.js
@@ -1,6 +1,6 @@
 var numbers=[1,2,3,4,5,6,10];
 
-var result=numbers.findIndex(function(currentValue, jndex, arr){
+var result=numbers.findIndex((currentValue, index, arr) =>{
     return currentValue >6;
 })
 console.log(result);
